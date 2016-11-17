85055ab939669aaeb7f6da692d5b0e5fbc3499ab
Jim Padilla and Tracie Weitzman <jim@>
2016-11-17 11:56:54 -0800

    change output, variable names for clearer UI

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -18,39 +18,42 @@ end
 class String
   define_method(:anagram_check) do |target_word, second_word|
     first_word = target_word.split("").sort().join("")
-    second_word = second_word.split("").sort().join("")
+    second_word_str = second_word.split("").sort().join("")
     booleanArray = []
 
-    if first_word == second_word
+    if first_word == second_word_str
       booleanArray.push(true)
     else
       booleanArray.push(false)
     end
 
-    booleanArray
+    second_word_boolean = booleanArray[0]
+    "#{second_word} is #{second_word_boolean}"
   end
 end
 
 class String
   define_method(:anagram_check2) do |target_word, second_word, third_word|
     first_word = target_word.split("").sort().join("")
-    second_word = second_word.split("").sort().join("")
-    third_word = third_word.split("").sort().join("")
+    second_word_str = second_word.split("").sort().join("")
+    third_word_str = third_word.split("").sort().join("")
     booleanArray = []
 
-    if first_word == second_word
+    if first_word == second_word_str
       booleanArray.push(true)
     else
       booleanArray.push(false)
     end
 
-    if first_word == third_word
+    if first_word == third_word_str
       booleanArray.push(true)
     else
       booleanArray.push(false)
     end
 
-    booleanArray
+    second_word_boolean = booleanArray[0]
+    third_word_boolean = booleanArray[1]
+    "#{second_word} is #{second_word_boolean}, #{third_word} is #{third_word_boolean}"
   end
 end
 
@@ -58,31 +61,34 @@ class String
   define_method(:anagram_check3) do |target_word, second_word, third_word, fourth_word|
     puts second_word, third_word, fourth_word
     first_word = target_word.split("").sort().join("")
-    second_word = second_word.split("").sort().join("")
-    third_word = third_word.split("").sort().join("")
-    fourth_word = fourth_word.split("").sort().join("")
+    second_word_str = second_word.split("").sort().join("")
+    third_word_str = third_word.split("").sort().join("")
+    fourth_word_str = fourth_word.split("").sort().join("")
     booleanArray = []
 
-    if first_word == second_word
+    if first_word == second_word_str
       booleanArray.push(true)
     else
       booleanArray.push(false)
     end
 
-    if first_word == third_word
+    if first_word == third_word_str
       booleanArray.push(true)
     else
       booleanArray.push(false)
     end
 
-    if first_word == fourth_word
+    if first_word == fourth_word_str
       booleanArray.push(true)
     else
       booleanArray.push(false)
     end
 
-    booleanArray
+    second_word_boolean = booleanArray[0]
+    third_word_boolean = booleanArray[1]
+    fourth_word_boolean = booleanArray[2]
+    "#{second_word} is #{second_word_boolean}, #{third_word} is #{third_word_boolean}, #{fourth_word} is #{fourth_word_boolean}"
   end
 end
 
-# puts 'cat'.anagram_check(['act', 'atc', 'tac'])
+ puts 'cat'.check_params(['act', 'atc', 'tac'])
